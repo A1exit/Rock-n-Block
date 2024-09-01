cb994101282084c52e15a851df57a6a463d3f1e3
alex <a1exitt@yandex.ru>
2024-09-01 16:38:24 +0300

    refactoring

--- rock_n_blok/api/migrations/0001_initial.py
+++ rock_n_blok/api/migrations/0001_initial.py
@@ -7,18 +7,50 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='Token',
+            name="Token",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('unique_hash', models.CharField(blank=True, max_length=20, unique=True, verbose_name='Уникальный хеш')),
-                ('tx_hash', models.CharField(blank=True, max_length=100, unique=True, verbose_name='Хэш транзакции создания токена')),
-                ('media_url', models.CharField(max_length=100, verbose_name='Картинка')),
-                ('owner', models.CharField(max_length=100, verbose_name='адрес пользователя в сети Ethereum')),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                (
+                    "unique_hash",
+                    models.CharField(
+                        blank=True,
+                        max_length=20,
+                        unique=True,
+                        verbose_name="Уникальный хеш",
+                    ),
+                ),
+                (
+                    "tx_hash",
+                    models.CharField(
+                        blank=True,
+                        max_length=100,
+                        unique=True,
+                        verbose_name="Хэш транзакции создания токена",
+                    ),
+                ),
+                (
+                    "media_url",
+                    models.CharField(max_length=100, verbose_name="Картинка"),
+                ),
+                (
+                    "owner",
+                    models.CharField(
+                        max_length=100,
+                        verbose_name="адрес пользователя в сети Ethereum",
+                    ),
+                ),
             ],
         ),
     ]
